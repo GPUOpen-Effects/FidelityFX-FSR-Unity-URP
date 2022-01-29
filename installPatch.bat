2d663f8433bad98ab2793cfccf29ec46069e6844
Fabricio-191 <fabricio19110@gmail.com>
2022-01-28 23:28:48 -0300

    Update installPatch.bat

--- installPatch.bat
+++ installPatch.bat
@@ -6,22 +6,28 @@ if ERRORLEVEL 1 (
 	goto end
 )
 
+if not exist library/PackageCache/ (
+	echo The script must be executed in a unity project
+	goto end
+)
 cd library/PackageCache/
-if exist com.unity.render-pipelines.universal goto install
 
-
-echo The unity project does not have an compatible URP (Universal Render Pipelines^)
-:option
-set /p continue= Try to install anyway ? (Y/N) 
-
-if %continue% == N exit
-if %continue% neq Y (
-	echo Invalid option
-	goto option
+if not exist "com.unity.render-pipelines.universal*" (
+	echo The unity project does not have any URP (Universal Render Pipelines^)
+	goto end
 )
 
-:install
-curl "https://raw.githubusercontent.com/GPUOpen-Effects/FidelityFX-FSR-Unity-URP/c96f5b0843cd56132693953f190e54eb93b58849/FSR1.0-For-URP10.6.0-Patch.patch" | git apply
+curl "https://raw.githubusercontent.com/GPUOpen-Effects/FidelityFX-FSR-Unity-URP/c96f5b0843cd56132693953f190e54eb93b58849/FSR1.0-For-URP10.6.0-Patch.patch" --output FSR1.0-URP10.6.0.patch
+
+for /D %%x in ("com.unity.render-pipelines.universal*") do (   
+	rename %%x com.unity.render-pipelines.universal 
+	
+	git apply FSR1.0-URP10.6.0.patch
+	
+	rename com.unity.render-pipelines.universal %%x
+)
+
+del /q FSR1.0-URP10.6.0.patch 
 
 :end
 echo:
